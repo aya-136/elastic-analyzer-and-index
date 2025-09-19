6dad6877f768b72dab6d73d8b5566a83a046ea36
aya-136 <145042635+aya-136@users.noreply.github.com>
2025-09-19 13:34:08 +0300

    Update TimedInvertedIndexCollection.java

--- elasticsearch-style-analyzer-aya-136/src/main/java/org/example/analyzer/index/TimedInvertedIndexCollection.java
+++ elasticsearch-style-analyzer-aya-136/src/main/java/org/example/analyzer/index/TimedInvertedIndexCollection.java
@@ -1,21 +1,29 @@
 package org.example.analyzer.index;
 
-import org.example.analyzer.Analyzer;
-
 import java.util.List;
 import java.util.Map;
 
-public class TimedInvertedIndexCollection extends InvertedIndexCollection {
-    public TimedInvertedIndexCollection(Analyzer analyzer) {
-        super(analyzer);
+public class TimedInvertedIndexCollection implements Index {
+
+    private final Index wrapped;
+
+    public TimedInvertedIndexCollection(Index wrapped) {
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public void addDocument(String docId, String text) {
+        wrapped.addDocument(docId, text); // <--- changed name here too
     }
 
     @Override
     public List<Map.Entry<String, Integer>> search(String query) {
         long start = System.nanoTime();
-        List<Map.Entry<String, Integer>> result = super.search(query);
+        List<Map.Entry<String, Integer>> result = wrapped.search(query);
         long end = System.nanoTime();
         System.out.println("Search for '" + query + "' took " + (end - start) / 1_000_000.0 + " ms");
         return result;
     }
 }
+
+
